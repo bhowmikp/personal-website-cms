b1cac4754bb260be9e63814a3117df15e75d8593
Prantar Bhowmik <prantar.bhowmik@rbc.com>
2021-03-22 23:45:52 -0400

    feat(blogs): added table of contents

--- schemas/blog.ts
+++ schemas/blog.ts
@@ -54,6 +54,36 @@ export default {
             validation: (Rule) => Rule.required().min(1, 'Need one or more').max(5, 'Too many'),
         },
         {
+            name: 'tableOfContents',
+            title: 'Table of Contents',
+            type: 'array',
+            of: [{
+                title: 'Section',
+                name: 'section',
+                type: 'object',
+                fields: [
+                    {
+                        name: 'sectionName', type: 'string', title: 'Section Name', validation: (Rule) => Rule.required()
+                    },
+                    {
+                        name: 'sectionId', type: 'string', title: 'Section ID', validation: (Rule) => Rule.required()
+                    },
+                    {
+                        name: 'sectionLevel', type: 'number',
+                        options: {
+                            list: [
+                                { title: 'level1', value: 1 },
+                                { title: 'level2', value: 2 },
+                                { title: 'level3', value: 3 }
+                            ],
+                            layout: 'radio'
+                        },
+                        validation: (Rule) => Rule.required()
+                    }
+                ]
+            }]
+        },
+        {
             name: 'body',
             title: 'Blog',
             type: 'richText',
