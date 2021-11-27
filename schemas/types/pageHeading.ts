8a1fbec883cc69fb64a2287e7310ce238af5da8e
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2021-11-26 19:24:27 -0500

    feat(stepper): added stepper to page cover (#21)

--- schemas/types/pageHeading.ts
+++ schemas/types/pageHeading.ts
@@ -7,6 +7,24 @@ export default {
     },
     fields: [
         {name: 'header', title: 'Header', type: 'string'},
-        {name: 'description', title: 'Description', type: 'text'}
+        {name: 'description', title: 'Description', type: 'text'},
+        {
+            name: "pageStepper",
+            title: "Page Stepper",
+            type: "array",
+            of: [{
+                title: "Stepper",
+                name: "stepper",
+                type: "object",
+                fields: [
+                    {
+                        name: "stepperLabel", title: "Stepper Label", type: 'string'
+                    },
+                    {
+                        name: "stepperLink", title: "Stepper Link", type: 'string'
+                    }
+                ]
+            }]
+        }
     ]
 }
