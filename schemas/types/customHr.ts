e771fb95a9d4194667e64eed0101b182667d24d5
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2021-12-15 22:11:56 -0500

    feat(ads): added ads

--- schemas/types/customHr.ts
+++ schemas/types/customHr.ts
@@ -3,10 +3,20 @@ import CustomHr from '../../components/CustomHr';
 export default {
     type: 'object',
     name: 'horizontalRow',
+    title: 'Horizontal Row',
     fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title',
+            validation: (Rule) => Rule.required(),
+            hidden: true,
+            initialValue: 'Horizontal Row'
+        },
         {
             type: 'string',
             name: 'horizontalRow',
+            title: 'Horizontal Row',
             inputComponent: CustomHr
         }
     ]
